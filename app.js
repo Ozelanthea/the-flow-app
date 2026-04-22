a260c15041c3e05a13df74e2992549d688bd51eb
Ozelanthea <ozelanthea@gmail.com>
2026-04-22 06:03:44 +0100

    Add task feature with empty input validation

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("JS is connected!")
+const btn = document.querySelector("#add-btn");
+const tasks = document.querySelector("#task-list");
+const input = document.querySelector("#task-input");
+
+btn.addEventListener("click", (event) => {
+
+    if (input.value === "") {
+        return;
+    }
+
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.textContent = input.value;
+
+    tasks.appendChild(item);
+    
+    input.value = "";
+})
+
+
